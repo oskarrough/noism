a0b4144b6b42dbbf7f006afb3b67792f80d97616
Oskar <oskarrough@gmail.com>
2017-01-26 10:49:20 +0100

    Add readme (#1)

--- src/untitled.js
+++ src/untitled.js
@@ -3,20 +3,20 @@ mouseMovement(0.08);
 function mouseMovement(moveAmount) {
 
 	bodyEl.mousemove(function( e) {
-
-	  	var cursorX = e.pageX;
-    	var cursorY = e.pageY;
-
-    	var moveX = (cursorX - (halfWindowWidth))*-moveAmount;
+		var cursorX = e.pageX;
+		var cursorY = e.pageY;
+		var moveX = (cursorX - (halfWindowWidth))*-moveAmount;
 		var moveY = (cursorY - (halfWindowHeight))*-moveAmount;
-		var rotateY = moveX*-0.09;
-    	TweenMax.to($('.figure-mover.active .js-figure-2'),
+		var rotateY = moveX * -0.09;
+
+		TweenMax.to($('.figure-mover.active .js-figure-2'),
 			1.175, {
 				x: moveX,
 				y: moveY,
 				rotationY: rotateY
 			}
 		);
+
 		TweenMax.to($('.figure-mover.active .js-figure'),
 			1.175, {
 				x: moveX*0.4,
@@ -25,5 +25,4 @@ function mouseMovement(moveAmount) {
 			}
 		);
 	});
-
 }
